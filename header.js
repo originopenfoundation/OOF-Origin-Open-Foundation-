6c901103628ead193c335677f60d91912310cef9
originopenfoundation <154004999+originopenfoundation@users.noreply.github.com>
2026-05-06 21:52:00 +0200

    Update header.js

--- header.js
+++ header.js
@@ -1,49 +1,3 @@
-function toggleStandards(){
-var menu = document.getElementById("standardsMenu");
-
-if(menu.style.display === "block"){
-menu.style.display = "none";
-}else{
-menu.style.display = "block";
-}
-}
-
-function toggleOOF(){
-var menu = document.getElementById("oofMenu");
-
-if(menu.style.display === "block"){
-menu.style.display = "none";
-}else{
-menu.style.display = "block";
-}
-}
-function toggleMIP(){
-  var menu = document.getElementById("mipMenu");
-
-  if(menu.style.display === "block"){
-    menu.style.display = "none";
-  }else{
-    menu.style.display = "block";
-  }
-}
-function toggleOS(){
-  var menu = document.getElementById("osMenu");
-
-  if(menu.style.display === "block"){
-    menu.style.display = "none";
-  }else{
-    menu.style.display = "block";
-  }
-}
-function toggleAcc(){
-  var menu = document.getElementById("AccMenu");
-
-  if(menu.style.display === "block"){
-    menu.style.display = "none";
-  }else{
-    menu.style.display = "block";
-  }
-}
 function toggleMenu(menuId) {
   const menus = ["standardsMenu", "oofMenu", "mipMenu", "osMenu", "AccMenu"];
 
@@ -51,10 +5,8 @@ function toggleMenu(menuId) {
     const el = document.getElementById(id);
     if (!el) return;
 
-    if (id === menuId) {
-      el.style.display = (el.style.display === "block") ? "none" : "block";
-    } else {
-      el.style.display = "none";
-    }
+    el.style.display = (id === menuId && el.style.display !== "block") 
+      ? "block" 
+      : "none";
   });
 }
