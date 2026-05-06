f1435b82661eb9c548badcd74909ad6e535370a6
originopenfoundation <154004999+originopenfoundation@users.noreply.github.com>
2026-05-06 21:28:35 +0200

    Update includes.js

--- includes.js
+++ includes.js
@@ -4,8 +4,3 @@ fetch('/header.html')
     document.getElementById('site-header').innerHTML = data;
   });
 
-fetch('/footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('site-footer').innerHTML = data;
-  });
