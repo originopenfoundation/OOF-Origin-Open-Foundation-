e1a23c8a350ca6b1a7aa26f9dd535787631370a1
originopenfoundation <154004999+originopenfoundation@users.noreply.github.com>
2026-05-20 19:37:45 +0200

    Update includes.js

--- includes.js
+++ includes.js
@@ -3,3 +3,9 @@ fetch('/header.html')
   .then(data => {
     document.getElementById('site-header').innerHTML = data;
   });
+
+fetch('/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('site-footer').innerHTML = data;
+  });
