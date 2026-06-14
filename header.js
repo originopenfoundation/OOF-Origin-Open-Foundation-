3bcb53ad7d67033e090c4b4ab554cb3024906ff2
originopenfoundation <154004999+originopenfoundation@users.noreply.github.com>
2026-06-14 12:30:59 +0200

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 function toggleMenu(menuId) {
-  const menus = ["standardsMenu", "authMenu", "oofMenu", "mipMenu", "resimMenu", "osMenu", "AccMenu", "GovernanceMenu", "govMenu", "engineMenu", "artdMenu", "burgerMenu", "archMenu", "simMenu", "FADMenu"];
+  const menus = ["standardsMenu", "authMenu", "oofMenu", "mipMenu", "resimMenu", "osMenu", "AccMenu", "GovernanceMenu", "govMenu", "engineMenu", "artdMenu", "burgerMenu", "archMenu", "simMenu", "FADMenu", "semanticMenu"];
 
   menus.forEach(id => {
     const el = document.getElementById(id);
