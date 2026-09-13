f09678f45b68775d9cfaecf3a8799141d4bc1f83
gomaeu <samuel.malicky2@gmail.com>
2026-09-13 22:49:12 +0200

    Fix Heat Map population data request

--- tools/update_global_interest_heat_map.py
+++ tools/update_global_interest_heat_map.py
@@ -22,7 +22,7 @@ PUBLIC_DATASET = ROOT / "data" / "oof-global-interest-heat-map.json"
 INTEREST_ALGORITHM_VERSION = "1.0"
 GRAPHQL_URL = "https://api.cloudflare.com/client/v4/graphql"
 POPULATION_METADATA_URL = "https://api.worldbank.org/v2/country?format=json&per_page=400"
-POPULATION_VALUES_URL = "https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json&per_page=40000&date=2020:2025"
+POPULATION_VALUES_URL = "https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json&per_page=5000&date=2020:2025"
 HOST = "originopenfoundation.org"
 WINDOW_DAYS = 28
 MIN_ENGAGEMENT = 10.0
